294e8d180e64c9c5f4cd484a0ca72ece53287efc
Wendell Bonto <wendell.bonto@gmail.com>
2018-10-10 20:02:40 +0800

    Dont Lab

---
+++ app/src/main/java/com/drawer/bonto/mobileprefilab/textfield_demo.java
@@ -0,0 +1,18 @@
+package com.drawer.bonto.mobileprefilab;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class textfield_demo extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.textfield_demo_layout, container, false);
+        return layout;
+    }
+}
